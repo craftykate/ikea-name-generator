09d5881c48876a2235e19d8efeafd9e3e2d623d2
Kate McFaul <katemcfaul@gmail.com>
2017-12-06 12:34:01 -0800

    Set up basic file structure

---
+++ src/Components/App/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <p>Hello World</p>
+    );
+  }
+}
+
+export default App;
